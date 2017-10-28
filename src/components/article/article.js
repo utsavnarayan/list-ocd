da1968a478979c5a4a141e5f6be788a6c1d39520
Utsav Singh <utsavinbox@gmail.com>
2017-10-28 07:59:51 +0530

    Added directory structure

---
+++ src/components/article/article.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './article.css';
+
+class Article extends Component {
+  render() {
+    return (
+      <div>
+        Article
+      </div>
+    );
+  }
+}
+
+export default Article;
